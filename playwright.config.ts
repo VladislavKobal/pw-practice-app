bbd9974a6d43021bff5e13cd89bf6340dc177da3
Vladyslav Kobal <vladyslav.kobal@intellias.com>
2024-03-21 20:32:15 +0200

    .

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   use: {
     headless: false,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://petstore.swagger.io/",
+    baseURL: "https://reqres.in/api",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
   },
